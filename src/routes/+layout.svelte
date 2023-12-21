22d9c7e3a3bd0707cb33729d19b49fd685dbfd5b
Luca D'Amico <damico.luca91@live.it>
2023-12-21 19:14:13 +0100

    fixed footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,17 +3,18 @@
     import { onMount } from 'svelte';
     import { createScene } from '$lib/scene';
     import Footer from '$lib/Footer.svelte'
-      let el:HTMLCanvasElement;
+    
+    let el:HTMLCanvasElement;
 
-  onMount(() => {
-    createScene(el);
-  });
+    onMount(() => {
+      createScene(el);
+    });
 </script>
   
   <div class="overflow-none flex flex-col w-screen h-screen">
       <canvas class="absolute" bind:this={el} />
       <main class="p-3 flex-1 relative overflow-auto">
       <slot></slot>
-      </main>
       <Footer />
+      </main>
   </div>
